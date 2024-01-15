909669ed815c2d32640ea68e9155bc3d64d548a9
Roberto Herencias Muñoz <54945514+Robertus06@users.noreply.github.com>
2024-01-15 11:58:25 +0100

    Add OnyxFighterCharacter Class

---
+++ Onyx/Source/Onyx/Public/Player/OnyxFighterCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../../OnyxCharacter.h"
+#include "OnyxFighterCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONYX_API AOnyxFighterCharacter : public AOnyxCharacter
+{
+	GENERATED_BODY()
+	
+};
