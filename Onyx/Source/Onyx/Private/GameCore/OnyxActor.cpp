add1b86566328347f7c2cf368074404f4da54e18
LuisTorres19 <luis.torres.valera.19@gmail.com>
2024-06-03 13:56:41 +0200

    OnyxActor

---
+++ Onyx/Source/Onyx/Private/GameCore/OnyxActor.cpp
@@ -0,0 +1,89 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameCore/OnyxActor.h"
+
+#include "GameplayEffectExtension.h"
+#include "AbilitySystem/OnyxAbilitySystemComponent.h"
+#include "AbilitySystem/OnyxAttributeSet.h"
+
+// Sets default values
+AOnyxActor::AOnyxActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	AbilitySystemComponent = CreateDefaultSubobject<UOnyxAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	OnyxAttributeSet = CreateDefaultSubobject<UOnyxAttributeSet>(TEXT("AttributeSet"));
+
+	InitializeAttributes();
+	InitializeEffects();
+}
+
+// Called when the game starts or when spawned
+void AOnyxActor::BeginPlay()
+{
+	Super::BeginPlay();
+	if(IsValid(AbilitySystemComponent))
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(OnyxAttributeSet->GetHealthAttribute()).AddUObject(this, &AOnyxActor::OnHealthAttributeUpdate);
+
+	}
+}
+
+UAbilitySystemComponent* AOnyxActor::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AOnyxActor::InitializeAttributes()
+{
+	if (!IsValid(AbilitySystemComponent))
+	{
+		return;
+	}
+
+	if (!DefaultAttributes)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s() Missing DefaultAttributes for %s. Please fill in the character's Blueprint."), *FString(__FUNCTION__), *GetName());
+		return;
+	}
+	FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+	AbilitySystemComponent->ApplyGameplayEffectToSelf(DefaultAttributes.GetDefaultObject(), 0, EffectContext);
+}
+
+void AOnyxActor::InitializeEffects()
+{
+	if (!IsValid(AbilitySystemComponent))
+	{
+		return;
+	}
+
+	for (auto Effect : InitialEffects)
+	{
+		FGameplayEffectContextHandle EffectContext = AbilitySystemComponent->MakeEffectContext();
+		EffectContext.AddSourceObject(this);
+
+		AbilitySystemComponent->ApplyGameplayEffectToSelf(Effect.GetDefaultObject(), 0, EffectContext);
+	}
+}
+
+// Called every frame
+void AOnyxActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AOnyxActor::OnHealthAttributeUpdate(const FOnAttributeChangeData& Data)
+{
+	HealthChangedEvent(Data.NewValue / OnyxAttributeSet->GetMaxHealth());
+
+	if(Data.NewValue <= 0.0f)
+	{
+		DeadEvent(Data.GEModData->EffectSpec.GetEffectContext().GetInstigator());
+	}
+
+}
+
---
+++ Onyx/Source/Onyx/Public/GameCore/OnyxActor.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
+#include "GameFramework/Actor.h"
+#include "OnyxActor.generated.h"
+
+UCLASS()
+class ONYX_API AOnyxActor : public AActor, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AOnyxActor();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual void InitializeAttributes();
+	virtual void InitializeEffects();
+
+	void OnHealthAttributeUpdate(const FOnAttributeChangeData& Data);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void HealthChangedEvent(float Percentage);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void DeadEvent(AActor* EffectInstigator);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	class UOnyxAbilitySystemComponent* AbilitySystemComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	const class UOnyxAttributeSet* OnyxAttributeSet;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UGameplayEffect> DefaultAttributes;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS", meta = (AllowPrivateAccess = "true"))
+	TArray<TSubclassOf<UGameplayEffect>> InitialEffects;
+};
